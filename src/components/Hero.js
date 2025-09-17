2b6783cba05f1571145494be9350c02daae9e4a5
MargaretTechworld <margarettechworld@gmail.com>
2025-09-17 01:27:42 +0000

    changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, lazy, Suspense } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { FaPhone, FaEnvelope } from 'react-icons/fa';
 import '../styles/Hero.css';
